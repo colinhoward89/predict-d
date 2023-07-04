b3c2ab3c181c5b8d1391d2b740beae6a13e4fb65
Colin Howard <colinhoward89@gmail.com>
2023-07-04 18:43:32 +0100

    Getting ready to deploy

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const dbName = process.env.DB_TEST || 'seedtest';
-const dbURL = process.env.DB_URL || 'mongodb://127.0.0.1:27017';
+const dbName = process.env.DB_TEST || 'predictd';
+const dbURL = process.env.DB_URI || 'mongodb://127.0.0.1:27017';
 
 const dbConnection = mongoose.connect(`${dbURL}/${dbName}`, {});
 

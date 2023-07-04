a0e0b686af357fdd22512989eb6175488fbff9aa
Colin Howard <colinhoward89@gmail.com>
2023-07-04 20:10:44 +0100

    deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,10 +10,16 @@ import User from './models/user';
 import populateDatabase from './asset/seedScript';
 
 const app: Express = express();
-const PORT: Number = Number(process.env.PORT) || 4000;
+const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-res.send('Hello!')
+export const config = {
+  server: {
+      port: PORT
+  }
+};
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Code with Rico. Ready to run on Heroku.');
 });
 
 (async () => {
@@ -27,7 +33,7 @@ res.send('Hello!')
 
 
     app.listen(PORT, () => {
-      console.log(`Server running at http://localhost:${PORT}`);
+      console.log(`Server running at ${config.server.port}`);
     });
   } catch (error) {
     console.error("Unable to connect to the database: ", error);
